2e5754c812e265f7e2cbb118dd1e57e89b0745fd
Lucas Barreto <bharretoprogrammer@gmail.com>
2020-03-31 10:55:43 -0300

    Update teste16.py

--- python/teste16.py
+++ python/teste16.py
@@ -1,6 +1,6 @@
-'''from math import trunc
+from math import trunc
 num = float (input("Digite um valor-"))
-print ("O valor digitado foi {} e a sua porção inteira é {}".format(num, trunc(num)))'''
+print ("O valor digitado foi {:.0f} e a sua porção inteira é {}".format(num, trunc(num)))
 
-num = float(input("Digite um valor- "))
-print ("O valor digitado foi {} e a sua porção inteira é {}".format (num, int(num)))
+'''num = float(input("Digite um valor- "))
+print ("O valor digitado foi {} e a sua porção inteira é {}".format (num, int(num)))'''
