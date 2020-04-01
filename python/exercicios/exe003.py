94d302fdff0befbe82a43a7dacb3f66bed25377f
Lucas Barreto <bharretoprogrammer@gmail.com>
2020-04-01 00:25:33 -0300

    update

---
+++ python/exercicios/exe003.py
@@ -0,0 +1,4 @@
+n1 = int (input("Digite um valor: "))
+n2 = int (input("Digite outro valor: "))
+sm = n1 + n2
+print ("A soma de {} mais {} é igual a {}".format(n1, n2, sm))
---
+++ python/exercicios/exe004.py
@@ -0,0 +1,7 @@
+b1a = int (input ("Primeira nota do Primeiro Bimestre: "))
+b1b = int (input ("Segunda nota do Primeiro Bimestre: "))
+b1c = int (input ("Terceira nota do Primeiro Bimestre: "))
+b1d = int (input ("Quarta nota do Primeiro Bimestre: "))
+average = (b1a + b1b + b1c + b1d)/4
+print ("A média das notas é {}".format (average))
+
