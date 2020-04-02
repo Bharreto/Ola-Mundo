8b60a9bc7fde2f489cd225dac3e659190390601f
Lucas Barreto <bharretoprogrammer@gmail.com>
2020-04-02 05:53:27 -0300

    update

---
+++ python/exercicios/exe007.py
@@ -0,0 +1,4 @@
+lado = float (input ("Qual é o valor do lado do quadrado: "))
+area = lado * lado
+area_aoquadrado = area * 2 
+print ("A área do quadrado é {} e o dobro do quadrado é {}".format (area,area_aoquadrado))
